41b9092cf839824b67445770fa5ffbfe39470dbd
Aakash Adhikari <aakash.adhikari@gmail.com>
2015-12-03 00:50:12 +0530

    UI
    register xml

--- app/src/main/java/com/ascentbrezie/brezie/activities/LoginOrRegisterActivity.java
+++ app/src/main/java/com/ascentbrezie/brezie/activities/LoginOrRegisterActivity.java
@@ -3,7 +3,6 @@ package com.ascentbrezie.brezie.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.transition.TransitionInflater;
 import android.util.Log;
 import android.view.View;
 
